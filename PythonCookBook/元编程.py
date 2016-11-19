b2f9540c8e2d1518208975a98f4fce0fad01b7d7
CUIPF993 <cpfyjjs@foxmail.com>
2016-11-19 16:05:38 +0800

    11.19

--- PythonCookBook/元编程.py
+++ PythonCookBook/元编程.py
@@ -318,7 +318,251 @@ s.bar(3)        #<__main__.Spam object at 0x030E3F30> 3
 import time
 from functools import wraps
 
+#A simple decorator
+def timethis(func):
+    '''
+    Decorator that reports the execution time
+    '''
+    @wraps(func)        #避免元数据丢失
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time()
+        print(func.__name__,end-start)
+        return result
+    return wrapper
 
+#Call illustrating application of the decorator to different kinds of method
+class Spam:
+    @timethis
+    def instance_method(self,n):
+        print(self,n)
+        while n > 0:
+            n -= 1
 
+    @classmethod
+    @timethis
+    def class_method(cls,n):
+        print (cls,n)
+        while n >0 :
+            n -= 1
+
+    @staticmethod
+    @timethis
+    def static_method(n):
+        print(n)
+        while n > 0:
+            n -= 1
+
+s = Spam()
+s.instance_method(1000)
+Spam.class_method(1000)
+Spam.static_method(1000)
+
+#9.11编写装饰器为包装的函数添加参数
+#我们想编写一个装饰器为被包装的函数添加额外的参数。但是添加的参数不能影响到该函数已有的调用约定
+#可以使用keyword-only参数将额外的参数注入到函数的调用签名中。
+
+from functools import wraps
+
+def optional_debug(func):
+    @wraps(func)
+    def wrapper(*args,debug = False,**kwargs):
+        if debug:
+            print('Calling',func.__name__)
+        return func(*args,**kwargs)
+    return wrapper
+
+@optional_debug
+def spam(a,b,c):
+    print(a,b,c)
+
+spam(1,2,3)
+#1,2,3
+spam(1,2,3,debug = True)    
+#Calling spam
+#1,2,3
+
+#9.12利用装饰器给类定义打补丁
+#我们想检查或修改一部分类定义，以此来修改类的行为，但是不想通过继承或者元类的方式实现
+#下面有一个装饰器重写了__getattribute__特殊方法，为其加上日志记录功能
+
+def log_getattribute(cls):
+    #Get the original implementation
+    orig_getattribute = cls.__getattribute__
+
+    #Make a new defination
+    def new_getattribute(self,name):
+        print('getting',name)
+        return  orig_getattribute(self,name)
+
+    #Attach to the class and return
+    cls.__getattribute__ = new_getattribute
+    return cls
+
+#Exaample 
+@log_getattribute
+class A:
+    def __init__(self,x):
+        self.x = x
+    def spam(self):
+        pass
+
+a = A(42)
+a.x     #getting x
+a.spam()        #getting spam
+
+#9.13利用元类来控制市里的创建
+
+#我们想改变实例的创建方式，以此来实现单例模式、缓存或者其他的特性
+#假设我们不想让任何人创建出实例：
+
+class NoInstance(type):
+    def __call__(self, **kwargs):
+        raise TypeError("Can't instantiate directly")
+class Spam(metaclass = NoInstance):
+    @staticmethod
+    def grok(x):
+        print('Spam.grok')
         
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+Spam.grok(42)       #Spam.grok
+#a = Spam()    #Can't instantiate directly    
+
+#假设我们想实现单例模式（即，这个只能创建唯一的一个实例）
+
+class Singleton(type):
+    def __init__(self, *args,**kwargs):
+        self.__instance = None
+        super().__init__(*args,**kwargs)  
+        
+    def __call__(self,*args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__(*args,**kwargs)
+            return self.__instance
+        else:
+            return self.__instance
+        
+#example 
+class Spam(metaclass = Singleton):
+    def __init__(self):
+        print('Creating Spam')
+
+#这个类只能创建出唯一的实例。        
+a = Spam()
+b = Spam()
+print(a is b)       #True   
+
+#假设我们想创建缓存实例。我们用一个元类实现
+
+import weakref
+
+class Cached(type):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.__cache = weakref.WeakValueDictionary()
+
+    def __call__(self,*args):
+        if args in self.__cache:
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+            return obj
+
+#Example
+class Spam(metaclass = Cached):
+    def __init__(self,name):
+        print('Creating Spam ({!r})'.format(name))
+        self.name = name
+
+a = Spam('Guido')
+b = Spam('Diana')
+c = Spam('Guido')
+print(a is b)       #False
+print(a is c)       #True
+
+#9.14获取类型属性的定义顺序
+#我们想自动记录下属性和方法在类中定义的顺序，这样就能利用这个顺序来完成各种各样操作（例如序列化处理、将属性映射到数据库中等）
+#要获取类定义体中的有关信息，可以通过元类来轻松实现。
+#下面的例子中，元类使用OrderedDict（有序字典）来获取描述符定义的顺序：
+
+from collections import OrderedDict
+
+#A set of descriptors for various types
+class Typed:
+    _expected_type = type(None)
+    def __init__(self,name = None):
+        self._name = name
+
+    def __set__(self,instance,value):
+        if not isinstance(value,self._expected_type):
+            raise TypeError('Expected '+str(self._expected_type))
+        instance.__dict__[self._name] = value
+
+class Integer(Typed):
+    _expected_type = int
+
+class Float(Typed):
+    _expected_type = float
+
+class String(Typed):
+    _expected_type = str
+
+#Metaclass that used an orderedDict for class body
+class OrderedMeta(type):
+    def __new__(cls,clsname,bases,clsdict):
+        d = dict(clsdict)
+        order = []
+        for name,value in clsdict.items():
+            if isinstance(value,Typed):
+                value._name = name
+                order.append(name)
+        d['_order'] = order
+        return type.__new__(cls,clsname,bases,d)
+
+    @classmethod
+    def __prepare__(cls,clsname,bases):
+        return OrderedDict()
+
+class Structure(metaclass = OrderedMeta):
+    def as_csv(self):
+        return ','.join(str(getattr(self,name))for name in self._order)
+
+#Example  use
+class Stock(Structure):
+    name = String()
+    shares = Integer()
+    price = Float()
+    def __init__(self,name,shares,price):
+        self.name = name 
+        self.shares = shares
+        self.price =price
+
+s = Stock('Good',100,23.33)
+#s = Stock('Good','100',23.33)
+#TypeError: Expected <class 'int'>
+"""
+本节的全部核心就在__prepare__()方法上，该特殊方法定义在元类OrderedMeta中，该方法会在类定义一开始的时候
+立刻得到调用，调用时以类名和基类名称作为参。它必须返回一个映射型对象(mapping object)供处理类定义体时使用。
+由于返回类型OrderDIct实例而不是普通字典，因此类中各个属性间的顺序就是可以方便地得到维护。
+"""
+
+#9.15定义一个接受可选参数的元类。
+#我们想定义一个元类，使得在定义类的时候提供可选的参数。这样的话在创建类型的时候可以对处理过程进行控制或配置
+
+#在定义类的时候，Python允许我们在class 语句中通过使用metaclass关键字参数来指定元类。
+
+from abc import ABCMeta
+from abc import abstractmethod
+
+class Istream(metaclass = ABCMeta):
+    @abstractmethod
+    def read(self,maxsize = None):
+        pass
+
+    @abstractmethod
+    def write(self,data):
+        pass
+
+
+
