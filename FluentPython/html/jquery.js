e935d2608fe8afc6ef7d0d8a4a264c60f1515739
cpfyjjs <cpfyjjs@foxmail.com>
2019-01-05 22:43:07 +0800

    05

---
+++ FluentPython/html/jquery.js
@@ -0,0 +1,28 @@
+
+$(function () {
+
+});
+$(document).read(function () {
+    $('button').click(function () {
+        $("p").hide();
+    });
+
+    $("#test");
+
+    $(".test");
+
+    $("ul li:first");
+    $("ul li:first-child");
+    $("[href]");
+    $("a[target='_brank']");
+    $("a[target!='_brank']");
+
+    $("#runoob").attr({
+        'href':'http://www.runoob.com/jquery',
+        'title':'jQuery 教程',
+    })
+
+});
+
+
+
